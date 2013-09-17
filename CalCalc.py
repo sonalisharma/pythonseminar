dc5210f83e191f3acac11d68050843a868f50a48
Sonali <sonali.sharma@ischool.berkeley.edu>
2013-09-17 02:30:05 -0700

    Added eval method, corrected errors in argparse

--- CalCalc.py
+++ CalCalc.py
@@ -1,10 +1,20 @@
 import argparse
+import BeautifulSoup
+import urllib2
+
 def calculate(userinput):
-	print "Testing command line argument"
+	try:
+		print eval(userinput)
+	except:
+		print "Error in evaluating expression"
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Evaluating command line expressions',
-									 help='Enter an expression to evaluate e.g. 3*4-5*6 or mass of moon in kgs')
-	parser.add_argument('-s', action="store")
-	results =  parser.parse_args()
-	calculate()
+	parser = argparse.ArgumentParser(description='Evaluating command line expressions')
+	parser.add_argument('-s', action="store",help='Enter an expression to evaluate e.g. 3*4-5*6 or "mass of moon in kgs" , make sure the string is provided within quotes')
+	try:
+		results =  parser.parse_args()
+		calculate(results.s)
+	except:
+		print "There is an error in your input, check help"
+		parser.print_help()
