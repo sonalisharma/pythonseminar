823f85c705e21f3c86e753a54b36dac61e41b020
Sonali <sonali.sharma@ischool.berkeley.edu>
2013-09-17 17:25:20 -0700

    Added nosetest functions, regular expression to eval, checking for edge cases

--- CalCalc.py
+++ CalCalc.py
@@ -1,12 +1,36 @@
 import argparse
 import BeautifulSoup
 import urllib2
+import re
 
 def calculate(userinput,return_float=False):
+	"""
+	This methos is used to read the user input and provide and answer. The answer is computed dircetly
+	using eval method if its a numerical expression, if not the wolfram api is used to get the appropriate answer.
+
+	Parameters:
+	userinput : This is a string, passed by the user in command line
+				e.g. "3*4+12" or "mass of moon in kgs"
+	return_float: This is to provide the output format, when specified as true then float is returned
+
+	Execution:
+	calculate("3*4+12", return_float=True)
+
+	Output:
+	The result is either a float or a string
+
+	"""
 	try:
-		ans  = eval(userinput)
-		return ans
-	except:
+		#This is to make sure user does not provide remve, delete or other sys commands in eval
+		#eval is used purely for numeric calculations here.
+		if (bool(re.match('.*[a-zA-Z].*', userinput, re.IGNORECASE))):
+			raise Exception ("Try with wolfram")
+		else:
+			ans  = eval(userinput)
+			if return_float:
+				ans = float(re.findall(r'\d+', ans))
+			return ans 
+	except Exception:
 		data = urllib2.urlopen('http://api.wolframalpha.com/v2/query?appid=UAGAWR-3X6Y8W777Q&input='+userinput.replace(" ","%20")+'&format=plaintext').read()
         soup = BeautifulSoup.BeautifulSoup(data)
         keys = soup.findAll('plaintext')
@@ -23,11 +47,11 @@ def test_1():
 def test_2():
 	assert calculate("total states in US") == '50'
 def test_3():
-	assert calculate("sin60 - sin0") == '50'
+	assert calculate("56*3+1000%2") == '168'
 def test_4():
-	assert calculate("total states in US") == '50'
+	assert 'Alaska' in calculate("largest state in US")
 def test_5():
-	assert calculate("total states in US") == '50'
+	assert '8' in calculate("planets in our solar system")
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Evaluating command line expressions')
@@ -35,7 +59,9 @@ if __name__ == '__main__':
 	try:
 		results =  parser.parse_args()
 		ans = calculate(results.s,return_float=True)
-		print "You Asked: %s"  %results.s
+		if ans=="":
+			ans="Sorry! No results found, try another question!"
+		print "You Asked: %s" %results.s
 		print "Answer: %s" %ans
 	except:
 		print "There is an error in your input, check help below"
