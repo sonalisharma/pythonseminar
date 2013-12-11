64b3017763df2d0eaa24d2aa27a48be83065f7b6
Sonali <sonali.sharma@ischool.berkeley.edu>
2013-12-11 02:24:52 -0800

    Added elements to analyse page, displaying plts based on user selected value

--- FinalProject/Frontend/mywebsite.py
+++ FinalProject/Frontend/mywebsite.py
@@ -74,8 +74,9 @@ def print_cv_score_summary(model, xx, yy, cv):
 def analyse():
     clf = linear_model.LogisticRegression()
     score = print_cv_score_summary(clf,data_features.values,data_target,cv=cross_validation.KFold(len(data_target), 10))
-    #features = data_features.columns
-    features = ["Count_comment","Count_funder", "Count_photo", "Mintotal", "Maxtotal", "Mediantotal"]
+    features = data_features.columns
+    print features
+    #features = ["Count_comment","Count_funder", "Count_photo", "Mintotal", "Maxtotal", "Mediantotal"]
     return render_template('analyse.html',score=score,features=features)
 
 def setstatus(ratio):
@@ -97,7 +98,7 @@ def fetchdata():
     return render_template('makematch.html')
 
 def getcountries():
-    countries = list(DATA['County'].unique())
+    countries = list(DATA['Country'].unique())
     return countries
 
 def getresult(userinput):
@@ -116,11 +117,75 @@ def getresult(userinput):
         status = "UnSuccessful"
     return status
 
+def gettitle(plotname):
+    plot = {"Count_update":["No. of updates",40],
+    "Count_comment":["No. of comments",200], 
+    "Count_funder":["No. of Funders",500],
+    "Count_photo":["No. of photos uploaded",60], 
+    "Amount_goal":["Amount to be raised",1000000] ,
+    "Start_month": ["Project start month",12],
+    "Duration":["Duration of Project",150], 
+    "Min_perk":["Amount of minimum perk($)",200], 
+    "No_min_perk":["No. of people contributing to min perk",100], 
+    "Max_perk":["Amount of maximum perk($)",50000], 
+    "No_max_perk":["No. of people contributing to max perk",50], 
+    "Median_perk":["Amount of median perk($)",2000], 
+    "No_med_perk":["No. of people contributing to median perk",100], 
+    "Tot_perk":["Total perks given",22], 
+    "FB_like":["Total FB likes",3000], 
+    "FB_talking":["Total FB shares",3000], 
+    "Youtube_avg_duratio":["Youtube video duration",2000], 
+    "Youtube_avg_view_count":["Youtube video count",5000000], 
+    "Youtube_avg_rating":["Youtube video avg ratings",6], 
+    "Mintotal":["Total money raised by min perk",50000], 
+    "Maxtotal":["Total money raised by max perk",20000]}
+    return plot.get(plotname)
+
+def drawplot(name):
+    g_data = gettitle(name)
+    print "This is it"
+    print g_data
+    cols = {1: "blue", 2: "green", 3: "red"}
+    label = {1}
+    xaxis = data_features[name]
+    amount_raised = DATA['Amount_Raised']
+    color = map(lambda x: cols[x],data_target['target'])
+    fig,ax1 = plt.subplots(figsize=(8,6))
+    ax1.scatter(xaxis,amount_raised,c = color, lw = 0.5)
+    ax1.set_title(g_data[0])    
+    ax1.set_xlabel(name)
+    ax1.set_ylabel('Amount Raised')
+    ax1.set_ylim([0,50000])
+    ax1.set_xlim([-5,g_data[1]])
+    line1 = plt.Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="blue")
+    line2 = plt.Line2D(range(1), range(1), color="white", marker='o',markerfacecolor="green")
+    line3 = plt.Line2D(range(1), range(1), color="white", marker='o',markersize=5, markerfacecolor="red")
+    plt.legend((line1,line2,line3),('Succesful','Moderately Successful', 'Unsuccessful'),numpoints=1, loc=1)
+    filepath = "static/img/Plots/"+name+".png"
+    fig.savefig(filepath) 
+    return  filepath
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    datajson = json.loads(request.form.get('data'))
+    vhtml = "<ul class=\"entries\">"
+    for data in datajson.values():
+        for d in data:
+            print "I am here this is "+d
+            filepath = drawplot(d)
+            vhtml =vhtml+"<li><img style=\"zoom: 0.5\" src=\""+filepath+"\"/></li>"
+    vhtml = vhtml+"</ul>"
+    return vhtml
+
+
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     countries = getcountries()
     predict = None
+    user_input = []
     if request.method == 'POST':
+        #try:
         comment_cnt = request.form["count_comment"]
         amount_goal = request.form["amount_goal"]
         funder_cnt = request.form["count_funder"]
@@ -129,12 +194,17 @@ def predict():
         max_perk = request.form["max_perk"]
         no_min_perk = request.form["no_min_perk"]
         no_max_perk = request.form["no_max_perk"]
-        user_input = [float(comment_cnt),float(amount_goal),float(funder_cnt),
-        float(photo_cnt),float(min_perk)*float(no_min_perk),float(max_perk)*float(no_max_perk)]
-        predict = getresult(user_input)
+        user_input = [float(amount_goal),float(comment_cnt),float(funder_cnt),
+        float(photo_cnt),float(min_perk),float(no_min_perk),float(max_perk),float(no_max_perk)]
+
+
+        predict = getresult([float(comment_cnt),float(amount_goal),float(funder_cnt),
+        float(photo_cnt),float(min_perk)*float(no_min_perk),float(max_perk)*float(no_max_perk)])
         print predict
-        return render_template('predict.html',countries=countries,prediction=predict)
-    return render_template('predict.html',countries=countries,prediction=predict)
+        return render_template('predict.html',countries=countries,prediction=predict,userinput=user_input)
+        #except:
+        #print "Exception occurred"
+    return render_template('predict.html',countries=countries,prediction=predict,userinput=user_input)
 
 if __name__ == "__main__":
     app.run()
--- FinalProject/Frontend/static/js/main.js
+++ FinalProject/Frontend/static/js/main.js
@@ -1,4 +1,31 @@
 $( document ).ready(function() {
+  console.log("Inside jquery");
+
+  var someGlobalArray = new Array;
+
+  $("input[type='checkbox']").click(function() {
+    console.log("I am here");
+      someGlobalArray=[];
+      $('#category:checked').each(function() {
+          someGlobalArray.push($(this).val());
+      });
+      console.log(someGlobalArray);
+        var data = {
+      data: JSON.stringify({
+                        "value":someGlobalArray
+                  })
+   };
+$.ajax({
+   url:"/data",
+   type: 'POST',
+   data: data,
+  success: function(msg){
+              var myDiv = $('.analyse_right'); // The place where you want to inser the template
+              myDiv.html("");
+              myDiv.html(msg);
+            }
+});
+  });
 
  
 
